b842b2ba0ae3139c16b280d5026f2f8a36162578
Tatiana <efimenko-ts@mail.ru>
2021-12-30 18:13:35 +0300

    fix: исправить ошибки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,34 @@
 const popupOpenButton = document.querySelector('.edit-btn');
 const popupCloseButton = document.querySelector('.popup__close');
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.popup');
+const userName = document.querySelector('.input__name');
+const userJob = document.querySelector('.input__job');
 
-function togglePopup () {
-  popup.classList.toggle('popup_opened');
-  nameInput.value = 'Жак-Ив Кусто';
-  jobInput.value = 'Исследователь океана';
+
+const formElement = document.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_job');
+const popupSaveButton = formElement.querySelector('.popup__save');
+
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
 }
-
-popupOpenButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
-
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
-let popupSaveButton = formElement.querySelector('.popup__save');
 
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let userName = document.querySelector('.input__name');
-  let userJob = document.querySelector('.input__job');
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
-  nameInput.value = 'Жак-Ив Кусто';
-  jobInput.value = 'Исследователь океана';
 }
 
-
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
 popupSaveButton.addEventListener('click', closePopup);
